57186580629a33fe1cce4d103b2c8b268072101c
yuchenliu <yuchenliu0815@gmail.com>
2017-11-29 18:25:19 +0800

    Edit the input text

--- yaoyao/task1/js/form.js
+++ yaoyao/task1/js/form.js
@@ -1,10 +1,20 @@
-var validate_inText = document.getElementById("validate_inText");
-validate_inText.onclick = function(){
-	function validate_inText(){
-		var inText = document.getElementById("inText");
-		var intext = new RegExp("[a-zA-Z]");
-		if(intext.length>=4||intext.length<=16){
-			alert(intext);
+function validate(){
+	var inText = document.getElementById("inText").value;
+	var length = 0;
+	for(var i =0;i<inText.length;i++){
+		if(inText[i].charCodeAt()>255||inText[i].charCodeAt()<0){
+			length += 2;
+		}else{
+			length++;
 		}
 	}
+	var result = document.getElementById("result");
+	if(length<4||length>16){
+		result.innerHTML = "长度必须大于4且小于16"
+		result.style.color = "red"
+	}else{
+		result.innerHTML = "通过！"
+		result.style.color = "green"
+	}
 }
+document.getElementById("validate_inText").onclick = function(){validate()}
