88f96dc1a236b3e3c58114b63edf9e80f0d85c9f
lychen <yuchenliu0815@gmail.com>
2017-12-16 11:50:08 +0800

    Update util.js

--- ife-2015/task2/util.js
+++ ife-2015/task2/util.js
@@ -1,16 +1,19 @@
+
+//判断arr是否为一个数组，返回boolean值
 function isArray(arr){
-    return Array.isArray(arr);//判断是否为一个数组
+  return Array.isArray(arr);
 }
-//深度克隆
-function cloneObject(src){
-    if(typeof src!=="object"||typeof src!=="function"){
-        return src;
+
+//利用递归实现深度克隆
+function deepClone(obj){
+  if(typeof obj !== "object" && typeof obj !== "function"){
+    return obj;         //原始类型直接返回
+  }
+  var o = isArray(obj)?[]:{};
+  for(var i in obj){
+    if(obj.hasOwnProperty(i)){
+      o[i] = typeof obj[i] === "object"? deepClone(obj[i]) : obj[i]
     }
-    var o = isArray(src)?[]:{};
-    for(var i in src){
-        if(src.hasOwnProperty(i)){
-            o[i] = typeof obj[i] === "object"?cloneObject(src[i]):obj[i]
-        }
-        return o;
-    }
+  }
+  return o;
 }
